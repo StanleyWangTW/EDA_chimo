d156d1d3c3bbdc4846fe2256d82fb4b9c2b9725e
X. S. Wang <47208731+StanleyWangTW@users.noreply.github.com>
2024-06-03 18:05:14 +0800

    Create cost.hpp

---
+++ cost.hpp
@@ -0,0 +1,13 @@
+#ifndef INCLUDE_H
+#define INCLUDE_H
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+double cost(int alpha, int beta, int gamma, int lambda) {
+  return 0;
+}
